278f30a86107cdbc2f1cfc4ee42b2469a0d19ce8
Rafael László <rlacko99@gmail.com>
2020-12-19 19:05:54 +0100

    day03 solution

--- test/day03_test.go
+++ test/day03_test.go
@@ -4,25 +4,27 @@ import (
 	"aoc-2020/internal/day03"
 	"aoc-2020/pkg/array"
 	"aoc-2020/pkg/input"
-	"fmt"
+	"testing"
 )
 
-// --- Day 3: Toboggan Trajectory ---
-// https://adventofcode.com/2020/day/3
 
-func main() {
-	rows := input.ReadStdinLines()
-	fmt.Println("Task1")
-	fmt.Println("Number of encountered trees:",
-		day03.CountEncounteredTrees(&rows, 3, 1))
+func TestDay03(t *testing.T) {
+	var t1i2Sol = 250
+	var t2i2Sol = 1592662500
 
-	fmt.Println("\nTask2")
+	rows := input.ReadFileLines("data/day03_2.txt")
+	ans := day03.CountEncounteredTrees(&rows, 3, 1)
+	if ans != t1i2Sol {
+		t.Errorf("CountEncounteredTrees(day03_2.txt, 3, 1) = %d, want %d", ans, t1i2Sol)
+	}
 	values := make([]int, 0)
 	values = append(values, day03.CountEncounteredTrees(&rows, 1, 1))
 	values = append(values, day03.CountEncounteredTrees(&rows, 3, 1))
 	values = append(values, day03.CountEncounteredTrees(&rows, 5, 1))
 	values = append(values, day03.CountEncounteredTrees(&rows, 7, 1))
 	values = append(values, day03.CountEncounteredTrees(&rows, 1, 2))
-	fmt.Println("Number of encountered trees:", values)
-	fmt.Println("Product:", array.ProductOfInts(&values))
+	ans = array.ProductOfInts(&values)
+	if ans != t2i2Sol {
+		t.Errorf("ProductOfInts(rows) = %d, want %d", ans, t2i2Sol)
+	}
 }
